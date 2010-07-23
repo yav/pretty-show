7abdfc6d82b9040cab6117ac116770d9a050d5d3
Iavor S. Diatchki <diatchki@galois.com>
2010-07-22 17:20:44 -0700

    Add a newline after prettyfying a value.

--- ppsh.hs
+++ ppsh.hs
@@ -6,14 +6,17 @@ main :: IO ()
 main =
   do as <- getArgs
      case as of
-       ["--test"] -> interact (show . selftest1)
-       []         -> interact $ \s -> case parseValue s of
-                                        Just v  -> show (ppValue v)
-                                        Nothing -> s
+       ["--test"] -> interactLn (show . selftest1)
+       []         -> interactLn $ \s -> case parseValue s of
+                                          Just v  -> show (ppValue v)
+                                          Nothing -> s
        _ -> hPutStrLn stderr $ unlines
               [ "usage: ppsh < showed_value > pretty_value"
               , "   --test      Self test: True means we passed."
               ]
+
+interactLn :: (String -> String) -> IO ()
+interactLn f = interact f >> putStrLn ""
 
 selftest :: Value -> Bool
 selftest v = case parseValue $ show $ ppValue v of

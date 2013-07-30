24a0f1f018a51ee57a07d08f1ae809f9dbd0c4c5
Dan Rosén <danr@chalmers.se>
2013-07-30 16:14:36 +0200

    Split test data type into two parts
    I think the reason is that derived Read instances are exponential in the
    size of the input string when using infix constructors.

--- test/Test.hs
+++ test/Test.hs
@@ -7,23 +7,31 @@ import Control.Monad
 
 import Control.Applicative
 
--- | A data type for testing read . ppShow = id
+infixr 4 :+:
+infixl 3 :$:
+infix 2 `Eq`
+
+-- | Testing infix constructors with different fixities
 data D
-    = A
-    | B D
-    | C D D
-    | D `I` D
-    | D :*: D
+    = X
+    | D `Eq` D
     | D :+: D
-    | D :=: D
     | D :$: D
-    | T (D,D)
-    | L [D]
-    | Int Int
+  deriving (Eq,Show,Read)
+
+infix :|:
+
+-- | Testing interopability between the different value constructs
+data D2
+    = K D2
+    | D2 :|: D2
     | Float Floaty
     | Char Char
     | String String
     | Ratio Rational
+    | Int Int
+    | L [D2]
+    | T (D2,D2)
   deriving (Eq,Show,Read)
 
 newtype Floaty = Floaty Float
@@ -42,49 +50,60 @@ instance Arbitrary Floaty where
     ,(1,return $ Floaty ((-1)/0))
     ]
 
-instance Arbitrary D where
-  arbitrary = sized arbD
-
-  shrink k = case k of
-      C x y   -> bin C x y
-      x `I` y -> bin I x y
-      x :*: y -> bin (:*:) x y
-      x :+: y -> bin (:+:) x y
-      x :=: y -> bin (:=:) x y
-      x :$: y -> bin (:$:) x y
-      T (x,y) -> bin (curry T) x y
-      L xs    -> xs ++ map L (shrink xs)
-      B x     -> x:map B (shrink x)
-      _       -> [A]
-    where
-      bin k x y = x:y:[ k a b | a <- shrink x, b <- shrink y ]
-
-
 arbD :: Int -> Gen D
 arbD s = frequency $
-    [ (1,return A)
-    , (1,Int <$> arbitrary)
-    , (1,Char <$> arbitrary)
-    , (1,String <$> arbitrary)
-    , (1,Ratio <$> arbitrary)
-    , (1,Float <$> arbitrary)
-    , (s,B <$> arbD (s - 1))
-    , (s,do
-        l <- choose (0,3)
-        L <$> replicateM l (arbD (s `div` (4-l)))
-      )
-    ]
+    [ (1,return X) ]
     ++
     [ (s,k <$> arbD s' <*> arbD s')
-    | k <- [C,I,(:*:),(:+:),(:=:),(:$:),curry T]
+    | k <- [Eq,(:+:),(:$:)]
     ]
  where
    s' = s `div` 2
 
-infixr 5 :*:
-infixr 4 :+:
-infixl 3 :$:
-infix 2 :=:
+arbD2 :: Int -> Gen D2
+arbD2 s = frequency $
+    [ (1,Int <$> arbitrary)
+    , (1,Char <$> arbitrary)
+    , (1,String <$> arbitrary)
+    , (1,Ratio <$> arbitrary)
+    , (1,Float <$> arbitrary)
+    , (s,K <$> arbD2 s')
+    , (s,do
+        l <- choose (0,3)
+        L <$> replicateM l (arbD2 (s `div` (5-l)))
+      )
+    ]
+    ++
+    [ (s,k <$> arbD2 s' <*> arbD2 s')
+    | k <- [(:|:),curry T]
+    ]
+ where
+   s' = s `div` 2
+
+instance Arbitrary D where
+  arbitrary = sized arbD
+
+  shrink k = case k of
+    Eq x y  -> shrinkBin Eq x y
+    x :+: y -> shrinkBin (:+:) x y
+    x :$: y -> shrinkBin (:$:) x y
+    _       -> [X]
+
+instance Arbitrary D2 where
+  arbitrary = sized arbD2
+
+  shrink k = case k of
+    x :|: y -> shrinkBin (:|:) x y
+    T (x,y) -> shrinkBin (curry T) x y
+    K x     -> shrinkUn K x
+    L xs    -> xs ++ map L (shrink xs)
+    _       -> [Int 0]
+
+shrinkUn :: Arbitrary a => (a -> a) -> a -> [a]
+shrinkUn k x = x:map k (shrink x)
+
+shrinkBin :: Arbitrary a => (a -> a -> a) -> a -> a -> [a]
+shrinkBin k x y = x:y:[ k a b | a <- shrink x, b <- shrink y ]
 
 prop_can_parse :: D -> Bool
 prop_can_parse = isJust . reify
@@ -92,8 +111,16 @@ prop_can_parse = isJust . reify
 prop_read_ppShow :: D -> Bool
 prop_read_ppShow x = (read . ppShow) x == x
 
+prop_can_parse2 :: D2 -> Bool
+prop_can_parse2 = isJust . reify
+
+prop_read_ppShow2 :: D2 -> Bool
+prop_read_ppShow2 x = (read . ppShow) x == x
+
 main :: IO ()
 main = do
     quickCheck prop_can_parse
-    quickCheck prop_read_ppShow
+    quickCheckWith stdArgs { maxSize = 25 } prop_read_ppShow
+    quickCheck prop_can_parse2
+    quickCheck prop_read_ppShow2
 

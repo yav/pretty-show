adea89b776fd09879f7f0f4923ca33b24207f50c
Iavor Diatchki <iavor.diatchki@gmail.com>
2018-10-24 14:21:40 -0700

    Add hiding support to the executable.

--- bin/ppsh.hs
+++ bin/ppsh.hs
@@ -1,11 +1,18 @@
 import Text.Show.Pretty
-import System.Environment
+import Data.Either(partitionEithers)
+import Data.List(isPrefixOf)
+import Data.Char(isSpace)
+import System.Environment(getArgs)
 import System.IO(hPutStrLn,stderr)
 
 main :: IO ()
 main =
   do as <- getArgs
-     case as of
+     let (opts,hiding) = partitionEithers (map isHiding as)
+         preproc v = case hiding of
+                       [] -> v
+                       xs -> hideCon False (`elem` hiding) v
+     case opts of
        ["--test"] -> interactLn (show . selftest1)
 
        ["--html"] ->
@@ -14,17 +21,26 @@ main =
               Just v  ->
                 do dir <- getDataDir
                    let opts = defaultHtmlOpts { dataDir = dir }
-                   putStrLn (valToHtmlPage opts v)
+                   -- XXX: perhaps for HTML the "hidden" values should
+                   -- just start off collapsed, rahter than being deleted?
+                   putStrLn (valToHtmlPage opts (preproc v))
               Nothing -> hPutStrLn stderr "Failed to parse value."
 
        []         -> interactLn $ \s -> case parseValue s of
-                                          Just v  -> show (valToDoc v)
+                                          Just v  -> show (valToDoc (preproc v))
                                           Nothing -> s
        _ -> hPutStrLn stderr $ unlines
               [ "usage: ppsh < showed_value > pretty_value"
+              , "   --hide=CON  Hide this constructor."
               , "   --html      Generate HTML."
               , "   --test      Self test: True means we passed."
               ]
+  where
+  isHiding a
+    | pref `isPrefixOf` a = Right $ dropWhile isSpace
+                                  $ drop (length pref) a
+    | otherwise = Left a
+    where pref = "--hide="
 
 
 interactLn :: (String -> String) -> IO ()
